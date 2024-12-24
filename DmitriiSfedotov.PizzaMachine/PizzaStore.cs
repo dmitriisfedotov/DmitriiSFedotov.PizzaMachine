930a7db56dc4ee7d0b139c6e0667172de3850d8f
dmitriisfedotov <dmitriisfedotov@icloud.com>
2024-12-24 14:22:43 +0300

    Optimize dictionary initialization

--- DmitriiSfedotov.PizzaMachine/PizzaStore.cs
+++ DmitriiSfedotov.PizzaMachine/PizzaStore.cs
@@ -1,20 +1,28 @@
 ﻿using DmitriiSFedotov.PizzaMachine.Enums;
 using DmitriiSFedotov.PizzaMachine.Exceptions;
 using DmitriiSFedotov.PizzaMachine.Pizzas;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 internal class PizzaStore
 {
+    private readonly Dictionary<PizzaType, IPizzaCreator> _creators = new Dictionary<PizzaType, IPizzaCreator>();
+    public PizzaStore()
+    {
+        _creators = new ()
+        {
+            [PizzaType.Pepperoni] = new PepperoniCreator(),
+            [PizzaType.Hawaiian] = new HawaiianCreator(),
+            [PizzaType.Margarita] = new MargaritaCreator(),
+            [PizzaType.FourCheeses] = new FourCheesesCreator()
+        };
+    }
     public async Task OrderPizza(PizzaType pizzaType)
     {
-        IPizzaCreator creator = pizzaType switch
+        if (!_creators.TryGetValue(pizzaType, out var creator))
         {
-            PizzaType.Pepperoni => new PepperoniCreator(),
-            PizzaType.Hawaiian => new HawaiianCreator(),
-            PizzaType.Margarita => new MargaritaCreator(),
-            PizzaType.FourCheeses => new FourCheesesCreator(),
-            _ => throw new IncorrectChoiceException("Некорректный выбор! Попробуйте снова!")
-        };
+            throw new IncorrectChoiceException("Некорректный выбор! Попробуйте снова!");
+        }
 
         IPizza pizza = creator.Create();
 
